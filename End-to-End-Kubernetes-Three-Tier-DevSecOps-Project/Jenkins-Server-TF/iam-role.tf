51d57b0834e64e6fba9f19fc0cb2af4e0a6f27be
SuryaPradeepthi <69852177+SuryaPradeepthi@users.noreply.github.com>
2024-09-04 16:03:06 -0500

    Add files via upload

---
+++ End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/iam-role.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "existing_role" {
+  name               = "Mernstack"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
